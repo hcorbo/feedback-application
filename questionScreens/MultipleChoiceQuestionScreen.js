349f43bd145a0b7f0f3d3612f41034e228477a01
msupuk <msupuk1@etf.unsa.ba>
2021-05-31 19:45:03 +0200

    Radio buttoni zamijenjeni obicnim buttonima

--- questionScreens/MultipleChoiceQuestionScreen.js
+++ questionScreens/MultipleChoiceQuestionScreen.js
@@ -1,42 +1,40 @@
 import React, { useState } from 'react'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import ButtonContainer from '../ButtonContainer';
-import { RadioButton } from 'react-native-paper';
-
+import {LinearGradient} from "expo-linear-gradient";
 
 const MultipleChoiceQuestionScreen = ({ question, navigation}) => {
   const [answers, setAnswers] = useState([]);
   var [checked, setChecked] = React.useState([]);
+  const [answersCount, setAnswersCount] = useState(question.QuestionAnswers);
 
   return (
-    <View>
-      <View style={styles.question}>
-        <Text style={styles.text_question}>{question.QuestionText}</Text>
-        <Text></Text>
-        <View style={styles.action}>
-          {
-            question.QuestionAnswers.map((answer) => (
-              <TouchableOpacity key={answer.AnswerId}>
-                <View style={styles.button_container}>
-               <View style={{flex:0}}>
-                  <RadioButton
-                    value={answer.AnswerId}
-                    status={ checked.find(element => element === answer.AnswerId) ? 'checked' : 'unchecked' }
-                    onPress={() => { checked.find(element => element === answer.AnswerId) ? (setChecked(prevState => (prevState.filter(element => element != answer.AnswerId)), setAnswers(prevState => (prevState.filter(element => element.AnswerId != answer.AnswerId))))) : (setChecked(prevState => ([...prevState, answer.AnswerId]), setAnswers(prevState => ([...prevState, {"QuestionId": question.QuestionId, "AnswerId": answer.AnswerId, "CustomAnswer": null}]))))}}
-                  />
-                </View> 
-                <View style={{flex:1, marginTop: 5}}>
-                  <Text style={styles.text_footer}>{answer.Answer.AnswerText}</Text> 
-                </View> 
-           </View> 
-                {/* <ChoiceComponent question={answer} /> */}
-              </TouchableOpacity>
-          ))
-          }               
+      <View>
+        <View style={styles.question}>
+          <Text style={styles.text_question}>{question.QuestionText}</Text>
+          <View style={styles.action}>
+            {
+              question.QuestionAnswers.map((answer) => (
+                  <TouchableOpacity
+                      key={answer.AnswerId}
+                      style={checked.includes(answer.AnswerId) ? [styles.pressed_button, {height: 80. / answersCount.length + '%'}] : [styles.unpressed_button, {height: 80. / answersCount.length + '%'}]}
+                      onPress={() => {
+                        checked.find(element => element === answer.AnswerId) ?
+                            (setChecked(prevState => (prevState.filter(element => element != answer.AnswerId)), setAnswers(prevState => (prevState.filter(element => element.AnswerId != answer.AnswerId))))) :
+                            (setChecked(prevState => ([...prevState, answer.AnswerId]), setAnswers(prevState => ([...prevState, {"QuestionId": question.QuestionId, "AnswerId": answer.AnswerId, "CustomAnswer": null}]))))
+                      }
+                      }
+                  >
+                    <LinearGradient colors={checked.includes(answer.AnswerId) ? ['#808080', '#808080'] : ['#ededed', '#d3d3d3']} style={[styles.button]}>
+                      <Text style={styles.text_answer}>{answer.Answer.AnswerText}</Text>
+                    </LinearGradient>
+                  </TouchableOpacity>
+              ))
+            }
+          </View>
         </View>
-     </View>
-      <ButtonContainer answer={answers} navigation={navigation}/>
-    </View>
+        <ButtonContainer answer={answers} navigation={navigation}/>
+      </View>
   )
 };
 
@@ -45,18 +43,17 @@ export default MultipleChoiceQuestionScreen;
 const styles = StyleSheet.create({
   question: {
     height: '90%',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
-  text_footer: {
-    color: '#fff',
-    fontSize: 18
+  text_answer: {
+    color: '#000',
+    fontSize: 25
   },
   action: {
     flexDirection: 'column',
     marginTop: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#f2f2f2',
-    paddingBottom: 5
+    paddingBottom: 5,
+    justifyContent: 'space-evenly'
   },
   button_container: {
     flexDirection: "row"
@@ -64,7 +61,25 @@ const styles = StyleSheet.create({
   text_question: {
     color: "#fff",
     paddingBottom: 40,
-    fontSize: 20,
+    fontSize: 25,
     alignSelf: 'center',
+  },
+  unpressed_button: {
+    alignSelf: 'center',
+    width: '100%',
+  },
+  pressed_button: {
+    alignSelf: 'center',
+    width: '100%',
+    borderWidth: 3,
+    borderRadius: 10
+  },
+  button: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderColor: '#000'
   }
 });
